280d19160b70e57cd92aa542fbfbb4fd8e97c36f
glaciapag <glenn.laciapag@gmail.com>
2022-12-25 20:32:58 +0800

    Refactored imports in init

--- src/pychemkit/__init__.py
+++ src/pychemkit/__init__.py
@@ -3,4 +3,5 @@ from pychemkit.foundations.element import Element
 from pychemkit.foundations.compound import Compound
 
 from pychemkit.stoich.reaction import SimpleChemicalReaction
-from pychemkit.stoich.experimental import EmpiricalFormula
+from pychemkit.stoich.experimental import EmpiricalFormula, MolecularFormula
+
